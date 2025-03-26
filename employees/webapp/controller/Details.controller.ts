312b5b6f04bad9a232064a8ef2c0271aa732ac8e
Hern� Quintero <hcquinteroz@gmail.com>
2025-03-26 14:53:54 +0000

    Inclusion del listado de ordenes por empleado

--- employees/webapp/controller/Details.controller.ts
+++ employees/webapp/controller/Details.controller.ts
@@ -8,12 +8,26 @@ import JSONModel from "sap/ui/model/json/JSONModel";
 
 export default class Details extends BaseController {
 
+    panel: Panel;
+
     public onInit(): void {
         const router = this.getRouter();
         router.getRoute("RouteDetails")?.attachPatternMatched(this.onObjectMatched.bind(this));
     }
 
+    // Model
+    private formModel(): void {
+        const model = new JSONModel([]);
+        this.setModel(model, "form");
+    }
+
     private onObjectMatched(event: Route$PatternMatchedEvent): void {
+
+        // Reset content from details
+        this.removeAllContent();
+
+        this.formModel();
+
         const modelView = <JSONModel>this.getModel("view");
         modelView.setProperty("/layout", "TwoColumnsMidExpanded");
 
@@ -32,5 +46,31 @@ export default class Details extends BaseController {
         const viewModel = <JSONModel>this.getModel("view");
         viewModel.setProperty("/layout", "OneColumn");
         router.navTo("RouteMaster");
+    }
+
+    private removeAllContent(): void {
+        const panel = <Panel>this.byId("tableIncidence");
+        panel.removeAllContent();
+    }
+
+    public async onCreatePress(): Promise<void> {
+        const panel = <Panel>this.byId("tableIncidence");
+
+        const formModel = <JSONModel>this.getModel("form");
+        const data = formModel.getData();
+        const index = data.length;
+        data.push({Index: index + 1});
+        formModel.refresh();
+
+        this.panel = await <Promise<Panel>> this.loadFragment({
+            name: "com.acme.employees.fragment.NewIncidence"
+        });
+
+        this.panel.bindElement({
+            path: 'form>/'+index,
+            model: 'form'
+        });
+
+        panel.addContent(this.panel);
     }
 }
